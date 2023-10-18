868ac7139c8e1428054bedb3e6ed15d30831bb7e
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-18 16:48:42 +0800

    merge

--- client/src/components/DashboardHeader.jsx
+++ client/src/components/DashboardHeader.jsx
@@ -8,7 +8,7 @@ export default function DashboardHeader({ toggleSidebar }) {
       {/* ======= Header ======= */}
       <header id="header" className="header fixed-top d-flex align-items-center">
         <div className="d-flex align-items-center justify-content-between">
-          <a href="index.html" className="logo d-flex align-items-center">
+          <a href="/dashboard" className="logo d-flex align-items-center">
             <img src="assets/images/logo-mc.png" alt="" />
             <span className="d-none d-lg-block">NiceAdmin</span>
           </a>
