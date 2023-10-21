459bf0f984fabfe698fed95a04ddba5001dd9c24
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-21 20:24:21 +0800

    complete signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,14 +1,51 @@
-import React, { useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'; // Import the Google logo icon
+import axios from 'axios';
 
 export default function Signup() {
+  const [formData, setFormData] = useState({}); // [1]
+  const [error, setError] = useState(false); // [1
+  const [loading, setLoading] = useState(false); // [1]
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value }); // [2]
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      setLoading(true);
+      setError(false);
+  
+      const response = await axios.post('/backend/auth/signup', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      const data = response.data;
+  
+      setLoading(false);
+  
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  
+
   useEffect(() => {
     const dashboardStylesheet = document.getElementById('dashboard-stylesheet');
-    dashboardStylesheet.setAttribute('disabled', 'true');
-    
+    dashboardStylesheet.setAttribute('disabled', true);
+
     const dashboardBootstrap = document.getElementById('dashboard-bootstrap');
-    dashboardBootstrap.setAttribute('disabled', 'true');
+    dashboardBootstrap.setAttribute('disabled', true);
   }, []);
 
   return (
@@ -41,8 +78,7 @@ export default function Signup() {
       <main className="main">
         <div className="height-100vh login-section signup-section position-relative bg-section bg-section-18">
 
-          <form className="sign-form">
-
+          <form onSubmit={handleSubmit} className="sign-form">
             <div className="form-heading text-center">
               <h4 className="sub-title ls-n-20 line-height-1 mb-3">Register Account</h4>
               <span className="heading-desc">
@@ -53,33 +89,34 @@ export default function Signup() {
               <div className="form-content">
                 <div className="input-group input-light">
                   <h6 className="input-title">Name</h6>
-                  <input type="text" className="form-control" placeholder="First Name" required />
-                  <input type="text" className="form-control" placeholder="Last Name" required />
-
+                  <input type="text" id='firstName' className="form-control" placeholder="First Name" required onChange={handleChange} />
+                  <input type="text" id='lastName' className="form-control" placeholder="Last Name" required onChange={handleChange} />
                 </div>
                 <div className="input-group input-light">
                   <h6 className="input-title">Email</h6>
-                  <input type="email" className="form-control" placeholder="example@gmail.com" required />
+                  <input type="email" id='email' className="form-control" placeholder="example@gmail.com" required onChange={handleChange} />
                 </div>
                 <div className="input-group input-light">
                   <h6 className="input-title">Password</h6>
-                  <input type="password" className="form-control" required />
+                  <input type="password" id='password' className="form-control" required onChange={handleChange} />
                 </div>
               </div>
             </div>
 
-            <button type="submit" className="btn btn-form btn-secondary-color">
-              <span>Sign Up</span>
+            <div className="term-privacy d-flex justify-content-center">
+              <span className="line-height-10">{error && "Email has been already exist!"}</span>
+            </div>
+
+            <button disabled={loading} type="submit" className="btn btn-form btn-secondary-color">
+              <span>{loading ? 'Signing Up...' : 'Sign Up'}</span>
             </button>
 
             <div className="term-privacy d-flex justify-content-center">
-              <span className="line-height-1">or </span>
+              <span className="line-height-1">or</span>
             </div>
-
             <button type="submit" className="btn btn-form btn-primary-color">
-              <span> <FontAwesomeIcon icon={faGoogle} className='google-icon' /> Sign up with Google</span>
+              <span><FontAwesomeIcon icon={faGoogle} className='google-icon' /> Sign up with Google</span>
             </button>
-
             <div className="term-privacy d-flex justify-content-center">
               <span className="line-height-1">Already have an account?</span>
               <div className="btn-link">
@@ -88,19 +125,12 @@ export default function Signup() {
                 </a>
               </div>
             </div>
-
           </form>
-
-
           <div className="foreground-img">
             <img src="assets/images/backgrounds/background-2-fore.png" alt="image" />
           </div>
         </div>
       </main>
     </>
-
-
-
-
-  )
+  );
 }
