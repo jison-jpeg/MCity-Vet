6e11cbb3958e23b15d62a7bb56fe8ba936872aec
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-21 17:26:20 +0800

    create signup api route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+    console.log(req.body);
+
+    const { firstName, lastName, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 12);
+    const newUser = new User({ firstName, lastName, email, password: hashedPassword });
+    try {
+        await newUser.save()
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+
+
+};
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log('Connected to MongoDB');
@@ -17,3 +20,4 @@ app.listen(3000, () => {
 });
 
 app.use("/backend/user", userRoutes);
+app.use("/backend/auth", authRoutes);
