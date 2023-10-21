814b10ee5ed3da2070bdf7ffc4d9ec232e2d58d7
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-21 14:57:29 +0800

    create user model

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,10 @@ const app = express();
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log('Connected to MongoDB');
-    }).catch(error => {
+}).catch(error => {
     console.log(error);
-    });
+});
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+    console.log('Server listening on port 3000');
 });
