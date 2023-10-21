658899aa1448f51868e1496aa5ba083fa62efb5b
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-21 23:12:56 +0800

    create sign in route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
 
 export const signup = async (req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
@@ -12,3 +15,23 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(401, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Invalid Credentials"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000);
+        
+        res
+        .cookie("token", token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true
     },
     lastName: {
         type: String,
-        required: true
     },
     email: {
         type: String,
@@ -19,13 +17,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-        required: true
+    role: {
+        type: String,
+        required: true,
+        enum: ['admin', 'technician', 'secretary', 'customer'],
+        default: 'customer'
     },
 }, { timestamps: true });
-
 
 const User = mongoose.model('User', userSchema);
 
