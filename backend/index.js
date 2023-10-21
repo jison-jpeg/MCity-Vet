cab33ebf476db4728d67a2b4d14292b9800e2a07
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-21 15:20:58 +0800

    create test backend route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 const app = express();
@@ -14,3 +15,5 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
+
+app.use("/backend/user", userRoutes);
