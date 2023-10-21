d06929abc2896446dc66b9549f2b41a08f0b9cd6
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-21 14:49:08 +0800

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Connected to MongoDB');
+    }).catch(error => {
+    console.log(error);
+    });
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
