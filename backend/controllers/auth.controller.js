d8ae5beb356cfc08aae5b0db074bde36cdc79a55
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-21 17:40:02 +0800

    add a middleware and functions to handle error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
-    console.log(req.body);
-
+export const signup = async (req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 12);
     const newUser = new User({ firstName, lastName, email, password: hashedPassword });
@@ -11,8 +9,6 @@ export const signup = async (req, res) => {
         await newUser.save()
         res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
-
-
 };
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import { error } from 'console';
 dotenv.config();
 
 const app = express();
@@ -21,3 +22,14 @@ app.listen(3000, () => {
 
 app.use("/backend/user", userRoutes);
 app.use("/backend/auth", authRoutes);
+
+
+app.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
