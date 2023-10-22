7b63d9b06ed6376d2f499ad256239705ff18321f
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-10-22 14:04:37 +0800

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
     <App />
+    </PersistGate>
   </Provider>,
+  
 )
