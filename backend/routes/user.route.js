714845c1781393cad1ae0c4d6ca2bff61d47f7a7
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-11-07 00:07:50 +0800

    Fix AccountManagement

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,22 +6,19 @@ import {
   deleteUser,
   getUserById,
   getAllUsers,
-  // getRoleStatistics,
-  // getTechnicians,
   getAppointmentsByUser,
-  
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
+router.get('/all', verifyToken, getAllUsers);
+router.get('/:id/appointments', verifyToken, getAppointmentsByUser);
+router.get('/:id', verifyToken, getUserById);
+
 router.get('/', test);
 router.post('/create', verifyToken, createUser);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
-router.get('/:id', verifyToken, getUserById);
-router.get('/all', verifyToken, getAllUsers);
-router.get('/:id/appointments', verifyToken, getAppointmentsByUser);
-
 
 export default router;
